b7d10145ae2bcc90f96f40a7ed2dd4339a5c5ef0
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2019-03-11 12:56:15 -0500

    App configurada con Firebase

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import firebase from 'firebase'
+  let config = {
+    apiKey: "AIzaSyCH8lsk4BnNEn4FWvlpXJDA5sTQzwXLS_c",
+    authDomain: "votacionescolsam.firebaseapp.com",
+    databaseURL: "https://votacionescolsam.firebaseio.com",
+    projectId: "votacionescolsam",
+    storageBucket: "",
+    messagingSenderId: "1035804809043"
+  };
+  firebase.initializeApp(config);
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
