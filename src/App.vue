acf4103c3fab41e96ffd073add0dede0eb14bd0e
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2019-03-11 13:08:58 -0500

    Añadiendo Toolbar informativo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-
+    <v-app>
+    <v-toolbar app color="primary" class="toolbar">
+      <v-toolbar-title class="logoMargin">
+        <v-img src="https://firebasestorage.googleapis.com/v0/b/colsamapp.appspot.com/o/logo%2Flogo.png?alt=media&token=d985d93c-2f39-4758-ba21-07fb86d99401" class="logo" height="66" width="54"></v-img>
+      </v-toolbar-title>
+      <v-toolbar-title class="white--text titulo hidden-md-and-down">
+        Votaciones Colsam 2019
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    </v-app>
   </div>
 </template>
 
