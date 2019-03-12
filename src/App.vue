921913e7a60fd69ccd240f96c99f7d0f3a700405
Dreft12 <jorgeelieceraldana.92@gmail.com>
2019-03-11 23:07:13 -0500

    Aplicacion funcionando correctamente

--- src/App.vue
+++ src/App.vue
@@ -10,50 +10,53 @@
                     Votaciones Colsam 2019
                 </v-toolbar-title>
                 <v-spacer></v-spacer>
+                <v-btn  flat class="white--text">Conteo de Votos</v-btn>
             </v-toolbar>
             <v-content>
-                <v-container fluid fill-height>
-                    <v-layout justify-start>
-                        <v-flex>
-                            <v-card width="250px">
-                                <v-img  class="white--text foto" offset-sm1 justify-center  align-center
-                                       src="https://firebasestorage.googleapis.com/v0/b/votacionescolsam.appspot.com/o/fotos%2F1.png?alt=media&token=3b558092-8993-4d04-b798-f74df7a4b51f">
+                <v-container fluid grid-list-md>
+                    <v-layout justify-center justify-space-around>
+                            <v-card width="350px">
+                                <v-img class="white--text foto"
+                                       src="https://firebasestorage.googleapis.com/v0/b/votacionescolsam.appspot.com/o/fotos%2F1.png?alt=media&token=deda4692-5e7f-43b0-adb4-25399a61f92e">
                                 </v-img>
                                 <v-card-title>
                                     <div>
-                                        <span class="grey--text">Janinna Isabel Baza Arrieta</span><br>
-                                        <span>"Para viajar lejos no hay mejor nave que un libro"</span><br>
+                                        <span>Janinna Isabel Baza Arrieta</span><br>
+                                        <span class="grey--text">"Para viajar lejos no hay mejor nave que un libro"</span><br>
                                     </div>
                                 </v-card-title>
                                 <v-card-actions>
-                                    <v-btn flat color="orange">Share</v-btn>
-                                    <v-btn flat color="orange">Explore</v-btn>
+                                    <v-btn color="primary" @click="votoJanina" block large>VOTAR</v-btn>
                                 </v-card-actions>
                             </v-card>
-                            <v-card width="400px">
-                                <v-img class="white--text foto" height="200px"
-                                       src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
-                                    <v-container fill-height fluid>
-                                        <v-layout fill-height>
-                                            <v-flex xs12 align-end flexbox>
-                                                <span class="headline">Opcion 01</span>
-                                            </v-flex>
-                                        </v-layout>
-                                    </v-container>
-                                </v-img>
-                                <v-card-title>
-                                    <div>
-                                        <span class="grey--text">Number 10</span><br>
-                                        <span>Whitehaven Beach</span><br>
-                                        <span>Whitsunday Island, Whitsunday Islands</span>
-                                    </div>
-                                </v-card-title>
-                                <v-card-actions>
-                                    <v-btn flat color="orange">Share</v-btn>
-                                    <v-btn flat color="orange">Explore</v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-flex>
+                        <v-card width="350px">
+                            <v-img class="white--text foto"
+                                   src="https://firebasestorage.googleapis.com/v0/b/votacionescolsam.appspot.com/o/fotos%2F3.png?alt=media&token=2bffe81b-2566-4c0b-a4c4-03e7288a65c7">
+                            </v-img>
+                            <v-card-title>
+                                <div>
+                                    <span>Kevin Sebastian Padilla Sarmiento</span><br>
+                                    <span class="grey--text">"Para viajar lejos no hay mejor nave que un libro"</span><br>
+                                </div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn color="primary" @click="votoKevin" block large >VOTAR</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        <v-card width="350px">
+                            <v-img class="white--text foto"
+                                   src="https://firebasestorage.googleapis.com/v0/b/votacionescolsam.appspot.com/o/fotos%2F2.png?alt=media&token=5e49df03-e517-44de-af62-b14deb181bf4">
+                            </v-img>
+                            <v-card-title>
+                                <div>
+                                    <span>Dairo José Quintana Olivares</span><br>
+                                    <span class="grey--text">"Para viajar lejos no hay mejor nave que un libro"</span><br>
+                                </div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn color="primary" @click="votoDairo" block large >VOTAR</v-btn>
+                            </v-card-actions>
+                        </v-card>
                     </v-layout>
                 </v-container>
             </v-content>
@@ -72,14 +75,33 @@
         storageBucket: "",
         messagingSenderId: "1035804809043"
     };
-    firebase.initializeApp(config);
-
+    let app =firebase.initializeApp(config);
+    let db = firebase.firestore(app);
     export default {
         name: 'app',
-        data() {
+        data: () => ({
 
-        },
-        methods: {}
+        }),
+        methods: {
+            votoJanina: function () {
+                let docRef = db.collection("personeros").doc('1001918552');
+                docRef.get().then(doc => {
+                    docRef.update({nrovotos: doc.data().nrovotos + 1});
+                });
+            },
+            votoDairo: function () {
+                let docRef = db.collection("personeros").doc('1002027132');
+                docRef.get().then(doc => {
+                    docRef.update({nrovotos: doc.data().nrovotos + 1});
+                });
+            },
+            votoKevin: function () {
+                let docRef = db.collection("personeros").doc('1002031194');
+                docRef.get().then(doc => {
+                    docRef.update({nrovotos: doc.data().nrovotos + 1});
+                });
+            }
+        }
     }
 </script>
 
